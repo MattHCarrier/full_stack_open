83a23ea78065191e669e6c6cdd9051c429eede85
matthcarrier <matieunibre183@gmail.com>
2021-11-16 16:47:10 -0800

    adding part1

---
+++ part1/src/App.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Hello = () => {
+    return(
+        <div>
+            <p>Hello World</p>
+        </div>
+    )
+}
+
+
+
+const App = () => {
+    return(
+     <div>
+         <h1>Greetings</h1>
+         <Hello />
+    </div>
+ )
+}
+
+export default App;
